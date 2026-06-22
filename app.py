0d1ece89c9c5c7a64fe8620f998bcc15160730ea
Tatiana Kawaguti <midori.kawaguti@yahoo.com.br>
2026-06-22 18:59:19 -0400

    Prepare app for deployment

--- app.py
+++ app.py
@@ -4,8 +4,14 @@ from dashboard.layout import create_layout
 from dashboard.callbacks import register_callbacks
 
 app = dash.Dash(__name__, suppress_callback_exceptions=True)
+server = app.server  # 👈 REQUIRED for deployment
+
 app.layout = create_layout()
 register_callbacks(app)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8050)
+    app.run_server(host='0.0.0.0', port=8050)
+
+#if __name__ == '__main__':
+    #app.run(debug=True, port=8050)
+   
